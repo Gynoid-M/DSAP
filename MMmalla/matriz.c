03d7ad6899ed40881bdceeb41d9a416a1d4cc7f3
Gynoid-M <marina.boluda@gmail.com>
2015-05-11 20:59:45 +0200

    Práctica terminada

--- MMmalla/matriz.c
+++ MMmalla/matriz.c
@@ -14,7 +14,7 @@ main(int argc, char **argv)
    int myrank, numprocs,numprocvalido = 0,bloqtam,bloqvalido=0, dimension,init = 0,usu = 0,destino,origen,sizeBuffer,errores;
    int fila,columna,i,k=0; 
    double *a,*b,*c,*buffer;
- 
+ double * atmp;
    
    int * mifila; 
 	
@@ -88,26 +88,30 @@ main(int argc, char **argv)
 			b= malloc(bloqtam*bloqtam*sizeof(double));
 			c= malloc(bloqtam*bloqtam*sizeof(double));
 			mifila=malloc((dimension - 1)*sizeof(int));
-			inicializar_matrices(a,b, dimension, bloqtam, fila, columna, i, myrank); 
+			 atmp = malloc(bloqtam*bloqtam*sizeof(double));
+			inicializar_matrices(a,b, c,dimension, bloqtam, 0, 0, i, myrank); 
 			 calcular_mifila(mifila,dimension,columna,fila,myrank);
-
+			
 			
 		}
-		else
+		else 
 		{
 			
 			
 			MPI_Recv(&bloqtam,1,MPI_INT,0,8,MPI_COMM_WORLD,&estado); 
 			MPI_Recv(&dimension,1,MPI_INT,0,8,MPI_COMM_WORLD,&estado); 
+	
 			 a = malloc(bloqtam*bloqtam*sizeof(double));
 			b= malloc(bloqtam*bloqtam*sizeof(double));
 			c= malloc(bloqtam*bloqtam*sizeof(double));
+			 atmp = malloc(bloqtam*bloqtam*sizeof(double));
 			mifila=malloc((dimension - 1)*sizeof(int));	
 
 			fila = myrank / dimension;
 			columna = myrank % dimension;
 
-			inicializar_matrices(a,b, dimension, bloqtam, fila, columna, i, myrank); 
+			inicializar_matrices(a,b,c, dimension, bloqtam, fila, columna, i, myrank); 
+
 			calcular_mifila(mifila,dimension,columna,fila,myrank);
 			
 		}
@@ -117,35 +121,47 @@ main(int argc, char **argv)
 		
 		MPI_Pack_size(pow(bloqtam,2),MPI_DOUBLE,MPI_COMM_WORLD, &sizeBuffer);
 		sizeBuffer = numprocs*(sizeBuffer + MPI_BSEND_OVERHEAD);
-		buffer = (double *)malloc(sizeBuffer);
+		buffer = malloc(sizeBuffer*sizeof(double));
 		MPI_Buffer_attach( buffer, sizeBuffer);
 
-		int tarea, calc_a = 1;	
+		int tarea;	
 		while(k < dimension) //donde k es el número de iteraciones 
 		{
+
+
+			
 			//Envío de A
 			
 				if(columna == (fila + k)% dimension)
 				{
 					
-					for(tarea = 0; tarea < dimension - 1; tarea++)
+					for(tarea = 0; tarea < dimension-1 ; tarea++)
 					{
-						MPI_Send(a,pow(bloqtam,2),MPI_DOUBLE,mifila[tarea],8,MPI_COMM_WORLD);
+					
+						MPI_Send(a,pow(bloqtam,2),MPI_DOUBLE,mifila[tarea],k,MPI_COMM_WORLD);
+						
 					}
+					
 					mult(a,b,c,bloqtam);
+					
+					
 				}
 				else
 				{
 					origen = ((fila + k)% dimension) + fila*dimension;
 					
-					int * atmp = malloc(bloqtam*bloqtam*sizeof(double));
-					MPI_Recv(atmp,pow(bloqtam,2),MPI_DOUBLE,origen,8,MPI_COMM_WORLD,&estado);
+
+					int copia;
+					
+
+					MPI_Recv(atmp,pow(bloqtam,2),MPI_DOUBLE,origen,k,MPI_COMM_WORLD,&estado);
+				
 					
 					mult(atmp,b,c,bloqtam);
-
-					free(atmp);
+					
+					
 				}
-				calc_a = 0;
+				
 			
 			//Envío de B
 		
@@ -155,7 +171,8 @@ main(int argc, char **argv)
 				}
 				else
 				{
-					destino = ((fila - 1) * dimension) + columna; //columna del emisor actual
+					
+					destino = myrank - dimension;
 					
 				}
 				if(fila == dimension - 1)
@@ -164,27 +181,35 @@ main(int argc, char **argv)
 				}
 				else
 				{
-					origen = ((fila + 1) * dimension) + columna;	
+					
+					origen = myrank + dimension;
 				}
 						
+				
 			
-				 MPI_Bsend(b,pow(bloqtam,2),MPI_DOUBLE,destino,8,MPI_COMM_WORLD);
-				 MPI_Recv(b,pow(bloqtam,2),MPI_DOUBLE,origen,8,MPI_COMM_WORLD,&estado);
-				 calc_a = 1;
-		       
+				 MPI_Bsend(b,pow(bloqtam,2),MPI_DOUBLE,destino,20*k,MPI_COMM_WORLD);
+				 MPI_Recv(b,pow(bloqtam,2),MPI_DOUBLE,origen,20*k,MPI_COMM_WORLD,&estado);
+
+			
+				
+		     
 			k++;
 
 		}
 		MPI_Buffer_detach(buffer, &sizeBuffer);
 
-		errores = check_igualdad(a,b,c,bloqtam);
+			
+		errores = check_igualdad(a,b,c,bloqtam,myrank);
 		//Comprobación de errores
 		if(myrank == 0)
 		{
+
+	
 			int tot_errores = 0;
-			for(i=0;i<numprocs;i++)
+			for(i=1;i<numprocs;i++)
 			{
-				MPI_Irecv(&errores,1,MPI_INT,i,8,MPI_COMM_WORLD,&request);
+				MPI_Recv(&errores,1,MPI_INT,i,100,MPI_COMM_WORLD,&request);
+				
 				printf("La cantidad de errores del proceso %d es %d\n",i, errores);
 				tot_errores = errores + tot_errores;
 			}
@@ -193,7 +218,7 @@ main(int argc, char **argv)
 		}
 		else
 		{
-			MPI_Send(&errores,1,MPI_INT,0,8,MPI_COMM_WORLD);
+			MPI_Send(&errores,1,MPI_INT,0,100,MPI_COMM_WORLD);
 		}
 	
 	MPI_Finalize(); 
@@ -204,41 +229,48 @@ void mult(double a[], double b[], double *c, int m)
 {
 	int i,j,k;
 	for (i=0; i<m; i++)
+	
 		for (j=0; j<m; j++)
+		
 			for (k=0; k<m; k++)
-				c[i*m+j]=c[i*m+j]+a[i*m+k]*b[k*m+j];
-	return; 
+			
+				c[i*m+j]=c[i*m+j]+ a[i*m+k]*b[k*m+j];
+			
+		
+	
+	
 }
 
-void inicializar_matrices(double * a , double * b, int dimension, int bloqtam, int fila, int columna, int i, int myrank)
+void inicializar_matrices(double * a , double * b, double *c, int dimension, int bloqtam, int fila, int columna, int i, int myrank)
 {
 			int tam = pow(bloqtam,2);
 			
 			
-			float pos = pow (((fila*columna)+1),2);
+			float pos = pow ((fila*columna+1),2);
 			
 			
-
 			int rellenob = 0;
-
-			for(i=0; i < (tam - 1) ; i++)
+			
+			for(i=0; i < tam  ; i++)
 			{ 
 				
 				a[i]=i*pos/tam;
-				
-				for(j = 0; i<(tam - 1); i++)
+
+				if (i == rellenob && fila == columna )
 				{
-					if(j == i)
-					{
-						b[i] = 1;
-					}
-					else
-					{
-						b[i] = 0;
-					}
-				}	
+
+					b[i] = 1;
+					rellenob = rellenob + bloqtam + 1;
+
+				}
+				else
+				{
+					b[i] = 0;
+				}
+				c[i] = 0;
 				
 			}
+			
 			
 		
 }
@@ -255,19 +287,25 @@ void calcular_mifila(int * mifila, int dimension, int columna,int fila, int myra
 		}
 	}
 }
-int check_igualdad(double * a,double *b, double * c, int bloqtam)
+int check_igualdad(double a[],double b[], double c[], int bloqtam,int myrank)
 {
 	int i;
 	int tam = pow(bloqtam,2);
 	int errores = 0;
+
+
 	for(i=0; i< tam ; i++)
 	{
 		
-		if(a[i] != c[i])
+		if(abs(a[i] - c[i] ) > 0.000000001)
 		{
 			errores++;
 		}
+
+		
 	}
+	
+				
 	
 	return errores;
 }
